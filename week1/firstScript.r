15a71d38d5f70484875e16a62ee1b101feaae780
kelaktaa <karim.elaktaa@gmail.com>
2017-04-08 20:51:36 +0200

    Introduction to quantitative investing - first commit (basic R scripting)

---
+++ week1/firstScript.r
@@ -0,0 +1,14 @@
+setwd("/home/kelaktaa/dev/MOOCs/quantitative-investing/week1/")
+
+# Read data
+SP500 <- read.csv(file = "SP500Data.csv")
+
+SP500
+
+colnames(SP500)
+
+price <- SP500$Close
+price2 <- SP500[,5]
+
+selectedData <- SP500[5, 3]
+selectedData
