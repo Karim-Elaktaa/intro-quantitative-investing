17fde2f72b388c08c335ccea44d0e71e3c8a1817
kelaktaa <karim.elaktaa@gmail.com>
2017-04-09 20:32:07 +0200

    Calculate geometric mean monthly return for SP500 and NASDAQ

--- week3/optimalPortfolio.R
+++ week3/optimalPortfolio.R
@@ -8,4 +8,18 @@ spData <- read.csv(file = "SP500Data.csv")
 # Get price data from data
 price <- data.frame(spData$Close, nasdaq$Close)
 colnames(price) <- c("SP500", "NASDAQ")
-head(price, n=10)
+
+# Rate of return & Annualized return
+gain <- price[1:(dim(price)[1] - 1),]
+cost <- price[2:(dim(price)[1]),]
+
+monthly.return <- (gain - cost) / cost
+
+number.month <- dim(monthly.return)[1]
+sprintf("Number of months %d", number.month)
+
+spData.monthly.return <- prod(1 + monthly.return$SP500) ^ (1/number.month) - 1
+sprintf("SP500 geometric average monthly return %f -- %f%%", spData.monthly.return, spData.monthly.return * 100)
+
+nasdaq.monthly.return <- prod(1 + monthly.return$NASDAQ) ^ (1/number.month) - 1
+sprintf("NASDAQ geometric average monthly return %f -- %f%%", nasdaq.monthly.return, nasdaq.monthly.return * 100)
